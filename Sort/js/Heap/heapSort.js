5dc2dbabdee2632c64e2c086151e7f66141d7180
ByteByBit <rajm.toth@gmail.com>
2019-11-16 11:38:29 +0100

    Create heapSort.js
    A JS file to demonstrate the heap sort algorithm.

---
+++ Sort/js/Heap/heapSort.js
@@ -0,0 +1,75 @@
+"use strict";
+class Heap {
+    constructor(inputArray) {
+        this.input = inputArray;
+        this.size = inputArray.length;
+        
+    }
+    // Get left or right node. 1 - left, 2 -right.
+    getNode(i, side) {
+        return 2 * i + side; 
+    }
+    
+}
+class HeapSort extends Heap{
+    
+    constructor( input ) {
+        super( input );        
+    }
+    main() {
+        console.log(this.input)
+        this.buildMaxHeap();
+        console.log(this.input)
+        // the first element is moved to the end and the next largest element is moved to the first place on each iteration
+        // the heapsize decreases because the end of the array become ordered
+        for (var j = this.size-1; j >= 0; j--) {
+            this.input.swap(0, j);
+            this.size--;
+            this.maxHeapify(0);
+        }
+
+        console.log(this.input);
+
+    }
+    buildMaxHeap() {
+        // we only need to process the first half of the array because all 
+        // element will be checked (as we check the
+        // right and left node)
+        var j = Math.floor(this.size / 2);
+        for ( j;j >= 0; j-- ) {
+            this.maxHeapify(j);
+        }
+    }
+        // the largest element between i, left(i) and right(i) will take the place of i
+        maxHeapify(i) {
+
+            var left = this.getNode(i, 1); // Left node.
+            var right = this.getNode(i, 2); // Right node.
+            var largest = i;
+    
+            if (left < this.size && this.input[left] > this.input[i]) {
+                largest = left;
+            }
+    
+            if (right < this.size && this.input[right] > this.input[largest]) {
+                largest = right;
+            }
+    
+            if (largest != i) {
+                this.input.swap(i, largest);
+                // we recursively call max heapify on the smallest element freshly moved so that it will moved in the array
+                // regarding its size
+                this.maxHeapify(largest);
+            }
+        }
+    
+        
+}
+Array.prototype.swap = function (x,y) {
+    var b = this[x];
+    this[x] = this[y];
+    this[y] = b;
+    return this;
+}
+var heapSort = new HeapSort([1,2,3,4, 9, 5]);
+heapSort.main();
